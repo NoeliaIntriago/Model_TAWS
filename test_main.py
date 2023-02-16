f811a2d0180d35ce9787a4092cf0fd84c8116aab
Joseph Avila Alvarez <josdavil@espol.edu.ec>
2023-02-16 18:05:36 -0500

    Fixed test directory

--- test_main.py
+++ test_main.py
@@ -1,8 +1,19 @@
 from fastapi.testclient import TestClient
-from .main import app
+import models.ml.classifier as clf
+from main import app
+
 
 client = TestClient(app)
 
+
 def test_read_main():
-    response = client.get("/")
-    assert response.status_code == 200
+    with TestClient(app) as client:
+        response = client.post("/predict",params={"param1":0,
+                                                "param2":0,
+                                                "param3":0,
+                                                "param4":0})
+        assert response.status_code == 200
+        assert response.json() == {"prediction": "Setosa",
+                                    "probability": [[1.0, 0.0, 0.0]]}
+        
+    
